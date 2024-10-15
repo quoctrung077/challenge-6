2b8036d374f2bd8c4b8978faaa131ea8b4ee843a
Kay Huynh <quoctrung077@gmail.com>
2024-10-15 11:28:54 +0700

    fix sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $('.btn__toggle').click(function () {
         $('.sidebar').toggleClass('collapsed');
-
-        // Hiển thị hoặc ẩn logo A và B
         if ($('.sidebar').hasClass('collapsed')) {
             $('.logo-a').fadeOut(400, function () {
                 $('.logo-b').fadeIn(400);
